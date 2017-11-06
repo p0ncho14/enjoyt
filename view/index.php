112f5aebe1136d3a6e6894dc588b2154010c32e8
CarlosArceo <master_carlos_1@hotmail.com>
2017-11-06 11:29:19 -0600

    Avances de lo que tengo
    Puede ser la estructura de la pagina

---
+++ view/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<title>Prueba</title>
+</head>
+<body>
+	<div class="alert alert-primary" role="alert">
+  This is a primary alert—check it out!
+</div>
+</body>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+</html>
